4485db1c1fe10846ee4fa1c5f05aed6652dd625a
chezbro <ericchesbrough@gmail.com>
2024-11-21 10:39:09 -0800

    redirect issue clerk

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,35 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-const protectedRoutes = createRouteMatcher(["/facetime(.*)", "/dashboard", "/"]);
+// Create a matcher for public routes
+const publicRoutes = createRouteMatcher(["/"])
 
-// protects the route
-export default clerkMiddleware((auth, req) => {
-    if (protectedRoutes(req)) {
-        auth().protect();
- }
+export default clerkMiddleware((auth, req: NextRequest) => {
+  const baseUrl = process.env.NEXT_PUBLIC_APP_URL;
+
+  // Allow public routes
+  if (publicRoutes(req)) {
+    return NextResponse.next();
+  }
+
+  // Handle users who aren't authenticated
+  if (!auth.userId && !publicRoutes(req)) {
+    const signInUrl = new URL('/sign-in', baseUrl);
+    signInUrl.searchParams.set('redirect_url', req.url);
+    return NextResponse.redirect(signInUrl);
+  }
+
+  // If the user is logged in and trying to access the sign-in page,
+  // redirect them to the dashboard
+  if (auth.userId && req.nextUrl.pathname === '/sign-in') {
+    const dashboard = new URL('/', baseUrl);
+    return NextResponse.redirect(dashboard);
+  }
+
+  return NextResponse.next();
 });
 
 export const config = {
-    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
